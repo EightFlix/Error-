1d538a95310f07db7e36877bfc4f57646b6d8b0a
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2025-12-22 14:43:53 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,20 @@
-# Credit - adarsh-goel
+# web/__init__.py
 
 from aiohttp import web
 from web.stream_routes import routes
 
 
-web_app = web.Application()
-web_app.add_routes(routes)
+# ======================================================
+# 🌐 WEB APP
+# ======================================================
+
+def create_app():
+    app = web.Application()
+
+    # routes
+    app.add_routes(routes)
+
+    return app
+
+
+web_app = create_app()
